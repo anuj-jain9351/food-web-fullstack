8c64484f0d8754c5f26cba4c5c12cdd127b8d178
Anuj Jain <anujchandaliya935129@gmail.com>
2026-06-30 23:50:34 +0530

    Hardcoded DB URI and pushing fix

--- Backend/Db-Conection/Db.js
+++ Backend/Db-Conection/Db.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const dbconnect = async()=>{
-    try{
-        await mongoose.connect(process.env.mongo_uri)
-        console.log("DB Connect")
-        
-    }catch(error){
-        console.log("DB Not Connect",error)
+const dbconnect = async () => {
+    try {
+        // Humne process.env hata kar seedhe aapka live link daal diya hai
+        await mongoose.connect("mongodb+srv://anujchandaliya935129:anuj1234@anujjain.dkta6am.mongodb.net/Food?retryWrites=true&w=majority&appName=AnujJain");
+        console.log("DB Connected Successfully");
+    } catch (error) {
+        console.log("DB Not Connect", error);
     }
 }
 
